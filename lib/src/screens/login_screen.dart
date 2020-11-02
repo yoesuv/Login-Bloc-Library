c0cec6217ca464183beea6e9a8a6dab1ace8d132
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-11-03 05:32:35 +0700

    :pencil: login screen widgets

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Center(
-          child: Text('Login Screen'),
+        child: Container(
+          margin: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              emailField(),
+              passwordField(),
+              Container(margin: EdgeInsets.only(top: 20.0)),
+              loginButton()
+            ],
+          ),
         )
     );
   }
+
+  Widget emailField() {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+            hintText: 'email@you.com',
+            labelText: 'Email Address',
+        )
+    );
+  }
+
+  Widget passwordField() {
+    return TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'your password',
+          labelText: 'Password',
+        )
+    );
+  }
+
+  Widget loginButton() {
+    return RaisedButton(
+      onPressed: (){},
+      child: Text('Login'),
+      color: Colors.teal,
+    );
+  }
+
 }
