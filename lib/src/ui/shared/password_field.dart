6356dd9cda308bcd92a995d025c13ccb46808180
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2020-11-07 16:30:43 +0700

    :pencil: toggle show/hide password

--- lib/src/ui/shared/password_field.dart
+++ lib/src/ui/shared/password_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PasswordField extends StatelessWidget {
+class PasswordField extends StatefulWidget {
 
   final Function onChange;
   final String errorMessage;
@@ -8,14 +8,38 @@ class PasswordField extends StatelessWidget {
   PasswordField({@required this.onChange, @required this.errorMessage});
 
   @override
+  PasswordFieldState createState() {
+    return PasswordFieldState();
+  }
+
+}
+
+class PasswordFieldState extends State<PasswordField>{
+
+  bool _isToggle = true;
+
+  void _passwordToggle() {
+    setState(() {
+      _isToggle = !_isToggle;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return TextField(
-        onChanged: onChange,
+        onChanged: widget.onChange,
         keyboardType: TextInputType.visiblePassword,
+        obscureText: _isToggle,
         decoration: InputDecoration(
             hintText: 'your password',
             labelText: 'Password',
-            errorText: errorMessage
+            errorText: widget.errorMessage,
+            suffixIcon: InkWell(
+              child: Icon(_isToggle ? Icons.visibility_off : Icons.visibility),
+              onTap: () {
+                _passwordToggle();
+              },
+            )
         )
     );
   }
