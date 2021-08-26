b19e3d1d0ade08b01492984ddabe72f7e7e6512e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2021-08-26 14:59:29 +0700

    :pencil: add fullname regex

--- lib/src/utils/validation/full_name_validation.dart
+++ lib/src/utils/validation/full_name_validation.dart
@@ -7,8 +7,13 @@ class FullName extends FormzInput<String, String> {
 
   @override
   String validator(String value) {
+    // https://stackoverflow.com/a/57289735
+    Pattern namePattern = r'^[a-z A-Z,.\-]+$';
+    RegExp regExp = new RegExp(namePattern, caseSensitive: false);
     if (value.isEmpty) {
       return 'Full Name is Empty';
+    } else if (!regExp.hasMatch(value.trim())) {
+      return 'Full Name is Not Valid';
     } else if (value.length >= 256) {
       return 'Full Name is To Long';
     } else {
