951bc550ec8d1e113b48b9d120b075e411e1b52e
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-09-26 05:35:57 +0700

    :hammer: refactor register screen

--- lib/src/ui/screens/register_screen.dart
+++ lib/src/ui/screens/register_screen.dart
@@ -9,27 +9,42 @@ import 'package:login_bloc_library/src/ui/shared/full_name_field.dart';
 import 'package:login_bloc_library/src/ui/shared/password_field.dart';
 import 'package:login_bloc_library/src/utils/app_helper.dart';
 
-class RegisterScreen extends StatelessWidget {
-
+class RegisterScreen extends StatefulWidget {
   static const routeName = "register";
 
   @override
+  State<StatefulWidget> createState() {
+    return _RegisterScreenState();
+  }
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  late RegisterBloc _bloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _bloc = context.read<RegisterBloc>();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final RegisterBloc bloc = context.read<RegisterBloc>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Register'),
       ),
       body: SafeArea(
         child: Container(
-          margin: EdgeInsets.all(16.0),
+          margin: EdgeInsets.all(16),
           child: Column(
             children: [
-              _fullNameInput(bloc),
-              _emailInput(bloc),
-              _passwordInput(bloc),
-              _confirmPasswordInput(bloc),
-              Container(margin: EdgeInsets.only(top: 20.0)),
+              _fullNameInput(),
+              _emailInput(),
+              _passwordInput(),
+              _confirmPasswordInput(),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+              ),
               _registerButton()
             ],
           ),
@@ -37,66 +52,76 @@ class RegisterScreen extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _fullNameInput(RegisterBloc bloc) {
-  return BlocBuilder<RegisterBloc, RegisterState>(
-    buildWhen: (previous, current) => previous.fullName != current.fullName,
-    builder: (context, state) {
-      return FullNameField(
-        onChange: (fullName) => bloc.add(FullNameChanged(fullName)),
-        errorMessage: state.fullNameError,
-      );
-    }
-  );
-}
+  Widget _fullNameInput() {
+    return BlocBuilder<RegisterBloc, RegisterState>(
+      bloc: _bloc,
+      buildWhen: (previous, current) => previous.fullName != current.fullName,
+      builder: (context, state) {
+        return FullNameField(
+          onChange: (fullName) => _bloc.add(FullNameChanged(fullName)),
+          errorMessage: state.fullNameError,
+        );
+      },
+    );
+  }
 
-Widget _emailInput(RegisterBloc bloc) {
-  return BlocBuilder<RegisterBloc, RegisterState>(
-    buildWhen: (previous, current) => previous.email != current.email,
-    builder: (context, state) {
-      return EmailField(
-        onChange: (email) => bloc.add(EmailChanged(email)),
-        errorMessage: state.emailError
-      );
-    },
-  );
-}
+  Widget _emailInput() {
+    return BlocBuilder<RegisterBloc, RegisterState>(
+      bloc: _bloc,
+      buildWhen: (previous, current) => previous.email != current.email,
+      builder: (context, state) {
+        return EmailField(
+          onChange: (email) => _bloc.add(EmailChanged(email)),
+          errorMessage: state.emailError,
+        );
+      },
+    );
+  }
 
-Widget _passwordInput(RegisterBloc bloc) {
-  return BlocBuilder<RegisterBloc, RegisterState>(
-    buildWhen: (previous, current) => previous.password != current.password,
-    builder: (context, state) {
-      return PasswordField(
-          onChange: (password) => bloc.add(RegisterPasswordChanged(password)),
-          errorMessage: state.passwordError
-      );
-    },
-  );
-}
+  Widget _passwordInput() {
+    return BlocBuilder<RegisterBloc, RegisterState>(
+      bloc: _bloc,
+      buildWhen: (previous, current) => previous.password != current.password,
+      builder: (context, state) {
+        return PasswordField(
+          onChange: (password) => _bloc.add(RegisterPasswordChanged(password)),
+          errorMessage: state.passwordError,
+        );
+      },
+    );
+  }
 
-Widget _confirmPasswordInput(RegisterBloc bloc) {
-  return BlocBuilder<RegisterBloc, RegisterState>(
-    buildWhen: (previous, current) => previous.passwordConfirm != current.passwordConfirm,
-    builder: (context, state) {
-      return PasswordField(
-          onChange: (password) => bloc.add(RegisterConfirmPasswordChanged(password)),
+  Widget _confirmPasswordInput() {
+    return BlocBuilder<RegisterBloc, RegisterState>(
+      bloc: _bloc,
+      buildWhen: (previous, current) =>
+          previous.passwordConfirm != current.passwordConfirm,
+      builder: (context, state) {
+        return PasswordField(
+          onChange: (password) => _bloc.add(
+            RegisterConfirmPasswordChanged(password),
+          ),
           errorMessage: state.passwordConfirmError,
           isConfirmPassword: true,
-      );
-    },
-  );
-}
+        );
+      },
+    );
+  }
 
-Widget _registerButton() {
-  return BlocBuilder<RegisterBloc, RegisterState>(
-    builder: (context, state) {
-      return ButtonPrimary(
+  Widget _registerButton() {
+    return BlocBuilder<RegisterBloc, RegisterState>(
+      bloc: _bloc,
+      buildWhen: (previous, current) => previous.status != current.status,
+      builder: (context, state) {
+        return ButtonPrimary(
           enable: state.status,
           onPress: () {
-            showToastSuccess('Submit Register');
+            showToastSuccess("Submit Register");
           },
-          text: 'Register');
-    },
-  );
+          text: "Register",
+        );
+      },
+    );
+  }
 }
